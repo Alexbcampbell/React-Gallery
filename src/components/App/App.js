9e4846259e919d886ad7e4ae53f94e0398cb873b
Alex Campbell <alexbe.campbell@gmail.com>
2020-10-30 16:50:18 -0500

    GET axios created on app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,54 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    galleryItem: {
+      id: '',
+      path: '',
+      description: '',
+      likes: '',
+    },
+    galleryList: [],
+    errorMsg: null,
+  };
+
+  componentDidMount() {
+    // component loaded and is ready
+    // get any data that I need
+
+    this.getGalleryData();
+  }
+
+  getGalleryData() {
+    axios({
+      method: 'GET',
+      url: '/gallery',
+    })
+      .then((response) => {
+        console.log('getGalleryData', this.state.galleryList);
+        this.setState({
+          galleryList: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({
+          errorMsg: 'Error in GET',
+        });
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of my life</h1>
         </header>
-        <br/>
+        <br />
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/goat_small.jpg" />
       </div>
     );
   }
