da7393b0e54dec411af45fb5bd243cc5029a5ab8
Alex Campbell <alexbe.campbell@gmail.com>
2020-10-31 20:26:47 -0500

    added 4 new image objects to gallery.data

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -11,6 +11,30 @@ const galleryItems = [
     description: 'A photo of me hiking in Utah',
     likes: 3,
   },
+  {
+    id: 3,
+    path: 'images/wedding.jpg',
+    description: 'My girlfriend and I at a wedding',
+    likes: 6,
+  },
+  {
+    id: 4,
+    path: 'images/colorado.jpg',
+    description: 'My girlfriend and I visiting St. Mary/s Glacier, Colorado',
+    likes: 5,
+  },
+  {
+    id: 5,
+    path: 'images/chiefs.jpg',
+    description: 'Chief/s snow game 2019',
+    likes: 3,
+  },
+  {
+    id: 6,
+    path: 'images/cabbage.jpg',
+    description: 'Our cat, Cabbage',
+    likes: 3,
+  },
 ];
 
 module.exports = galleryItems;
