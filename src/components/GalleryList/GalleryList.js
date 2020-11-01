69d55416682871a7c738216a5c866b7a2680c145
Alex Campbell <alexbe.campbell@gmail.com>
2020-10-31 20:38:46 -0500

    installed bootstrap CSS, added flex row styling to GalleryList

--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './GalleryList.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class GalleryList extends Component {
   render() {
     return (
-      <div>
+      <div className="d-flex flex-row bd-highlight mb-3">
         {this.props.gallery.map((item, index) => (
-          <div key={index}>
+          <div key={index} className="p-2 bd-highlight">
             <p>{item.id}</p>
             <img src={item.path} alt="photos of my life" className="img" />
             <p>{item.description}</p>
