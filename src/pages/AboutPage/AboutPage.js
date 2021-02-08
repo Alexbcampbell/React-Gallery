58131a9b50ebb16f06380178aaae360a3549743c
Alex Campbell <alexbe.campbell@gmail.com>
2021-02-08 00:13:23 -0600

    added button and route to gallery from about page

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 
 class AboutPage extends Component {
+  clickForGallery = () => {
+    console.log('toggled');
+    this.props.history.push('/gallery');
+  };
   render() {
     return (
       <div>
         <h2>About</h2>
         <p>Welcome to my gallery</p>
         <p>Feel free to browse my pictures and click the like button!</p>
+        <button onClick={this.clickForGallery}>
+          Click here to see the Gallery!
+        </button>
       </div>
     );
   }
